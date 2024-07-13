e0c0304bbcd3bdf3eb97703f81b7198c136f6ab2
meDracula <me.dracula@protonmail.com>
2024-07-13 13:20:28 +0200

    Multistage with hightechsec/web-ctf-container to build custom web site

---
+++ ctf2/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Cracken</title> 
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <h1 class="text-center">
+                <picture><img src="https://github.com/meDracula/meDracula/blob/main/images/developer.gif?raw=true" width=50px></picture>
+                PHP 🐙 Challenges
+                <picture><img src="https://github.com/meDracula/meDracula/blob/main/images/developer.gif?raw=true" width=50px></picture>
+            </h1>
+        </div>
+        <br>
+        <br>
+        <!-- Challenge #1 -->
+        <div class="container-fluid">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Strcmp Bypass</h3>
+                </div>
+                <div class="panel-body">
+                    The idiot Albin have created a website where he store his secrets... Unfortunately, we cant login into his website 🙀
+                    <br>
+                    Help me!
+                </div>
+                <div class="panel-footer">
+                    <a href="/admin/index.php" target="_blank" class="btn btn-success">Start here</a>
+                    <a href="/admin/index.php?debug" target="_blank" class="btn btn-primary">Source code</a>
+                </div>
+            </div>
+        </div>
+        <script
+              src="https://code.jquery.com/jquery-3.1.1.min.js"
+              integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+              crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </body>
+</html>
